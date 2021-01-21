0fbd1fb163487ebe756e48a4459fa0d6412c4002
Elijah Meshnick <emeshnick@gmail.com>
2021-01-21 17:33:17 -0500

    fix: testing heroku

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Heist</h1>
     <nav>
       {isLoggedIn ? (
         <div>
