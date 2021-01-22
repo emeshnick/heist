b45d57ab45ecfb31277e52aea8ed4d21cfe471c1
Elijah Meshnick <emeshnick@gmail.com>
2021-01-22 13:46:20 -0500

    fix: changed index.html to be accessed by heroku

--- server/db/db.js
+++ server/db/db.js
@@ -7,7 +7,6 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
-    // operatorsAliases: false,
     dialect: 'postgres',
     protocol: 'postgres',
     ssl: true,
