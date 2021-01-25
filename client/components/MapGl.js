15c56c7062e7986ed39f7424e8b2611fbb35d781
Elijah Meshnick <emeshnick@gmail.com>
2021-01-24 20:11:17 -0500

    changed map to use react-map-gl

--- client/components/MapGl.js
+++ client/components/MapGl.js
@@ -1,74 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 import {fetchMarkers, addMarker} from '../store/markers'
-import mapboxgl from 'mapbox-gl'
+// import mapboxgl from 'mapbox-gl'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl'
 import socket from '../socket'
 
-mapboxgl.accessToken =
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoiZW1lc2huaWNrIiwiYSI6ImNraHVzdjZ0cjFhdHUycG5wMzM4YjQxNm8ifQ.tmoY8tVhd9okCvVgWwZapQ'
+const mapboxToken =
   'pk.eyJ1IjoiZW1lc2huaWNrIiwiYSI6ImNraHVzdjZ0cjFhdHUycG5wMzM4YjQxNm8ifQ.tmoY8tVhd9okCvVgWwZapQ'
-
 class MapGl extends React.Component {
   constructor() {
     super()
-    this.markers = []
+    this.mapBoxMarkers = []
     this.addMarker = this.addMarker.bind(this)
     this.makeGroupMarker = this.makeGroupMarker.bind(this)
+
+    this.state = {
+      viewport: {
+        latitude: 35.9,
+        longitude: -79,
+        zoom: 9,
+        bearing: 0,
+        pitch: 0,
+      },
+    }
   }
 
   componentDidMount() {
     //Create map and add to 'map' div
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-79, 35.9],
-      zoom: 9
-    })
-
-    //To locate user on map
-    this.map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        trackUserLocation: true
-      })
-    )
-
+    // this.map = new mapboxgl.Map({
+    //   container: 'map',
+    //   style: 'mapbox://styles/mapbox/streets-v11',
+    //   center: [-79, 35.9],
+    //   zoom: 9,
+    // })
+    // //To locate user on map
+    // this.map.addControl(
+    //   new mapboxgl.GeolocateControl({
+    //     positionOptions: {
+    //       enableHighAccuracy: true,
+    //     },
+    //     trackUserLocation: true,
+    //   })
+    // )
     //Create handler for user to add marker
-    this.map.on('click', event => {
-      const coords = `lat: ${event.lngLat.lat} <br> lng: ${event.lngLat.lng}`
-      // const popup = new mapboxgl.Popup().setText(coords)
-      // new mapboxgl.Marker()
-      //   .setLngLat(event.lngLat)
-      //   .setPopup(popup)
-      //   .addTo(this.map)
-      this.addMarker({
-        coords: coords,
-        lngLat: event.lngLat
-      })
-      this.makeGroupMarker(coords, event.lngLat)
-      socket.emit('marker', {coords: coords, lngLat: event.lngLat})
-    })
-
-    //Socket to receive marker data and add to map
-    socket.on('marker', data => {
-      this.addMarker(data)
-    })
-
-    console.log('props are', this.props)
-    this.props.conversation.id &&
-      this.props.fetchMarkers(this.props.conversation.id)
+    // this.map.on('click', (event) => {
+    //   const coords = `lat: ${event.lngLat.lat} <br> lng: ${event.lngLat.lng}`
+    //   this.addMarker({
+    //     coords: coords,
+    //     lngLat: event.lngLat,
+    //   })
+    //   this.makeGroupMarker(coords, event.lngLat)
+    //   socket.emit('marker', {coords: coords, lngLat: event.lngLat})
+    // })
+    // //Socket to receive marker data and add to map
+    // socket.on('marker', (data) => {
+    //   this.addMarker(data)
+    // })
+    // console.log('props are', this.props)
+    // this.props.conversation.id &&
+    //   this.props.fetchMarkers(this.props.conversation.id)
   }
 
+  //Helper function to use to add marker to local map
   addMarker(marker) {
-    const popup = new mapboxgl.Popup().setText(marker.coords)
-    const addedMarker = new mapboxgl.Marker()
-      .setLngLat(marker.lngLat)
-      .setPopup(popup)
-      .addTo(this.map)
-    addedMarker.getElement().addEventListener('click', () => {
-      console.log('clicked marker ', marker)
-    })
+    // const popup = new mapboxgl.Popup().setText(marker.coords)
+    // const addedMarker = new mapboxgl.Marker()
+    //   .setLngLat(marker.lngLat)
+    //   .setPopup(popup)
+    //   .addTo(this.map)
+    // addedMarker.getElement().addEventListener('click', () => {
+    //   console.log('clicked marker ', marker)
+    // })
+    // this.mapBoxMarkers.push(addedMarker)
   }
 
   makeGroupMarker(coords, lngLat) {
@@ -76,29 +81,38 @@ class MapGl extends React.Component {
       conversationId: 1,
       lngLat: lngLat,
       coords: coords,
-      descriptions: []
+      descriptions: [],
     }
     this.props.addMarker(newMarker)
   }
 
   render() {
     const {markers} = this.props
-    markers.map(marker => this.addMarker(marker))
-    return <div id="map" />
+    markers.map((marker) => this.addMarker(marker))
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={mapboxToken}
+      />
+    )
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     conversation: state.conversation,
-    markers: state.markers
+    markers: state.markers,
   }
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
-    fetchMarkers: conversationId => dispatch(fetchMarkers(conversationId)),
-    addMarker: marker => dispatch(addMarker(marker))
+    fetchMarkers: (conversationId) => dispatch(fetchMarkers(conversationId)),
+    addMarker: (marker) => dispatch(addMarker(marker)),
   }
 }
 
